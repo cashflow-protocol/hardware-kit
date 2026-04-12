05384f933c945faeb3f361370afe61a6b6e3b1e9
heymike <mike@dangervalley.com>
2026-04-12 12:14:17 +0200

    Cancel button fix for Keystone

--- sdk/react-native-ui/src/components/DefaultQrInteractionHandler.tsx
+++ sdk/react-native-ui/src/components/DefaultQrInteractionHandler.tsx
@@ -142,7 +142,6 @@ export function useQrInteractionHandler(
                 </Text>
                 <QrCodeScanner
                   onScan={handleScanResult}
-                  onCancel={handleCancel}
                   renderScanner={options?.renderScanner}
                 />
               </>
